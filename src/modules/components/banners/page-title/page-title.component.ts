b091e34e677a76f421a42e70e9a0febb56b7c529
Vygintas Ranonis <vygintas.ranonis@gmail.com>
2016-05-20 14:31:02 +0100

    added transclusion to page title component

--- src/modules/components/banners/page-title/page-title.component.ts
+++ src/modules/components/banners/page-title/page-title.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@govuk/angularjs-devtools';
   bindings: {
     title: '@'
   },
+  transclude: {
+    subtitle: '?subtitle'
+  },
   template: require('./page-title.component.html')
 })
 export class PageTitleComponent {}
