c5f411628257bab381e0096c24d47ab4fe3d8e8e
Ben Ripley <ben.ripley@digi2al.co.uk>
2016-05-23 09:38:04 +0100

    feat(alert): port alert box from ngGovUk

---
+++ src/modules/components/banners/alert/alert.demo.ts
@@ -0,0 +1,10 @@
+import { Component } from '@govuk/angularjs-devtools';
+
+@Component({
+  template: `
+    <docs-example markup>
+      <div class="gov-alert" role="alert">This is an alert</div>
+    </docs-example>
+  `
+})
+export class AlertDemo {}
