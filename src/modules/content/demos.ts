00e5479bcb00dee4e5f2dcb004ff22f474e49025
Ben Ripley <ben.ripley@digi2al.co.uk>
2016-05-23 16:11:09 +0100

    feat(form-elements): port search box from ngGovUk

--- src/modules/content/demos.ts
+++ src/modules/content/demos.ts
@@ -2,12 +2,14 @@ import { content } from './content';
 import { docsUtils } from '../../util/docs';
 import { RadioDemo } from './form-elements/radio/radio.demo';
 import { CheckboxDemo } from './form-elements/checkbox/checkbox.demo';
+import { SearchDemo } from './form-elements/search/search.demo';
 
 const module = angular.module('govuk-single-page-pdk.content-demos', [
   content,
   docsUtils
 ])
   .component('govRadioDemo', RadioDemo)
-  .component('govCheckboxDemo', CheckboxDemo);
+  .component('govCheckboxDemo', CheckboxDemo)
+  .component('govSearchDemo', SearchDemo);
 
 export const contentDemos = module.name;
