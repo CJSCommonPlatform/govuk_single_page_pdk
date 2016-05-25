f57254db0b6c47984a0f8dc2c8c097578b60bca3
James GRANT <james.grant@ext.soprasteria.com>
2016-05-25 11:47:14 +0100

    Icons and media

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -18,6 +18,7 @@ import { ColoursPage } from './app/content/colours.component';
 import { HomePage } from './app/home/home.component';
 import { LayoutPage } from './app/layout/layout.component';
 import { FormElementsPage } from './app/content/form-elements/form-elements.component';
+import { IconsMediaPage } from './app/content/icons-media.component';
 
 angular.module('govuk-single-page-pdk.docs', [
   config,
@@ -41,4 +42,5 @@ angular.module('govuk-single-page-pdk.docs', [
   .component('govDocsExamplesPage', ExamplesPage)
   .component('govDocsLayoutPage', LayoutPage)
   .component('govDocsContentColoursPage', ColoursPage)
-  .component('govDocsFormElementsPage', FormElementsPage);
+  .component('govDocsFormElementsPage', FormElementsPage)
+  .component('govDocsContentIconsMediaPage', IconsMediaPage);
