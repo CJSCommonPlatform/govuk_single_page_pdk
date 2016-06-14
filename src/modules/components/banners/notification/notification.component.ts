0a68763ccd48f8ace70550b50e1fd2599973446f
Ben Ripley <ben.ripley@digi2al.co.uk>
2016-06-14 11:53:55 +0100

    refactor(components/notification): show icon by default and inherit icon from notification type

--- src/modules/components/banners/notification/notification.component.ts
+++ src/modules/components/banners/notification/notification.component.ts
@@ -3,15 +3,23 @@ import { Component } from '@govuk/angularjs-devtools';
 @Component({
   bindings: {
     type:     '@',
-    showIcon: '@'
+    showIcon: '<'
   },
   transclude: true,
   template: `
     <div class="gov-notification {{$ctrl.type}}">
-      <i class="gov-icon gov-icon-important" ng-if="$ctrl.showIcon"></i>
+      <i class="gov-icon" ng-if="$ctrl._showIcon"></i>
       <div class="gov-notification-body" ng-transclude>
       </div>
     </div>  
   `
 })
-export class NotificationComponent {}
+export class NotificationComponent {
+
+  showIcon:  boolean;
+  _showIcon: boolean;
+
+  $onChanges() {
+    this._showIcon = this.showIcon === undefined ? true : this.showIcon;
+  }
+}
