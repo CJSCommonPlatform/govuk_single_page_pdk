1ba2dd444320a221b145ced1afc3377acdeebf64
Vygintas Ranonis <vygintas.ranonis@ext.soprasteria.com>
2016-06-17 14:30:22 +0100

    added demo for side-menu component

--- src/modules/components/navigation/menus/side-menu/side-menu.demo.ts
+++ src/modules/components/navigation/menus/side-menu/side-menu.demo.ts
@@ -6,145 +6,86 @@ import { Component } from '@govuk/angularjs-devtools';
 export class SideMenuDemo {
 
   menuGroup = {
-    heading: 'Contents',
+    heading: 'Menu Group Heading',
+    subheading: 'Clicking on an item will activate it, deactivate all other items, and ' +
+    'log a \'did you know\' message with the ' +
+    'fact property of the current menu item, unless it is already active or disabled.',
     menus: [
       {
+        heading: 'Items with arrows',
         showArrowsOnItems: true,
         items: [
           {
-            text: 'Suspects and charges',
-            sref: 'suspects-and-charges',
+            text: 'Item 1',
+            fact: 'stewardesses is the longest word that is typed with only the left hand',
             isActive: true
           },
           {
-            text: 'Victims, witnesses and statements',
-            sref: 'suspects-and-charges'
+            text: 'Item 2',
+            fact: 'M&M\'s chocolate stands for the initials for its inventors Mars and Murrie'
           },
           {
-            text: 'Additional documents',
-            sref: 'suspects-and-charges',
+            text: 'Item 3',
+            fact: 'Koalas sleep around 18 hours a day',
             isDisabled: true
           }
         ]
-      }
-    ]
-  };
-
-  menuGroup2 = {
-    heading: 'Suspects and charges',
-    subheading: '2 items',
-    menus: [
+      },
       {
+        heading: 'Items with content, without arrows',
         items: [
           {
-            title: 'Peter JAMES',
-            text: 'Single proposed charge',
-            isActive: true
+            title: 'Item 1 title',
+            fact: 'elephants are the only mammal that can\'t jump',
+            text: 'Single line string'
           },
           {
-            title: 'John BROWN',
-            text: 'Single proposed charge'
-          }
-        ]
-      }
-
-    ]
-  };
-
-  menuGroup3 = {
-    heading: 'Police summary',
-    subheading: '6 items',
-    menus: [
-      {
-        items: [
-          {
-            title: 'Summary of key evidence',
-            isActive: true
-          },
-          {
-            title: 'Defendant interview'
-          },
-          {
-            title: 'Injuries'
-          },
-          {
-            title: 'Previous convictions'
-          },
-          {
-            title: 'Court orders and compensation'
-          },
-          {
-            title: 'Supervising officer’s commments'
-          }
-        ]
-      }
-    ]
-  };
-
-  menuGroup4 = {
-    heading: 'Victims, witnesses and statements',
-    subheading: '2 items',
-    menus: [
-      {
-        heading: 'Victims',
-        items: [
-          {
-            title: 'Linda THOMAS',
-            text: ['Vulnerable', '1 statement'],
-            isActive: true
+            title: 'Item 2 title',
+            fact: 'on your birthday you share it with 9 million others',
+            text: ['Multiline text', 'array of strings']
           }
         ]
       },
       {
-        heading: 'Witnesses',
+        heading: 'Item with an image',
         items: [
           {
-            title: 'Kasia HRYNISZAK',
-            text: '2 statements'
-          }
-        ]
-      }
-    ]
-  };
-
-  menuGroup5 = {
-    heading: 'Exhibits',
-    subheading: '3 items',
-    menus: [
-      {
-        heading: 'Photos',
-        items: [
-          {
-            title: 'Text is centred vertically when image is taller',
+            title: 'Image title',
+            text: 'Image 1 text',
             image: 'images/justice.jpg',
-            isActive: true
-          },
-          {
-            image: 'images/justice.jpg',
-            title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' +
-              'Nulla pellentesque ante id lectus condimentum tempor.'
+            fact: 'hummingbirds are the only bird that can fly backwards'
           }
         ]
       },
-      {
-        heading: 'Videos',
-        items: [
-          {
-            title: 'CCTV from street camera'
-          }
-        ]
-      }
     ]
   };
 
+  /*the component has no internal logic but allows a number of actions on the controller/service
+  level by exposing both the entire menuGroup object, and the currently clicked menuItem object,
+  on the onMenuItem click attribute.
+  Note that in the html, the naming of the arguments is important:
+    on-menu-item-click="$ctrl.onMenuItemClick(menuGroup, menuItem)"
+  Also, since the menuGroup object is passed by reference, it is possible to access and modify it
+  from other contexts, eg. passing it to a function which activates the next/previous item, and
+  attaching the function on pagination components.
+  */
   onMenuItemClick = function (menuGroup, menuItem) {
-    if (menuItem.isDisabled) {
-      return false;
+    if (!(menuItem.isDisabled || menuItem.isActive)) {
+      this.logFact(menuItem.fact);
+      this.activateItem(menuGroup, menuItem);
     }
+  };
+
+  activateItem = function (menuGroup, menuItem) {
     menuGroup.menus.forEach(function (menu) {
       menu.items.forEach(function (item) {
         item.isActive = item === menuItem;
       });
     });
   };
-}
+
+  logFact = function (fact) {
+    console.log('Did you know that ' + fact + '?');
+  }
+  
+};
