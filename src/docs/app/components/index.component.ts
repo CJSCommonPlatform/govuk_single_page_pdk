a6a5ec1e7977581e598e7fc58aa0015716021703
Fabio Tisci <fabio.tapia@ext.soprasteria.com>
2016-06-21 15:05:41 +0100

    added documentation on missing components

--- src/docs/app/components/index.component.ts
+++ src/docs/app/components/index.component.ts
@@ -9,14 +9,14 @@ export class ComponentsPage {
         {
             name: 'Data-Visualisation',
             state: 'components.data-visualisation',
-            components: ['Contact-info', 'Personal-details', 'Summary-item', 'Tab']
+            components: ['Contact-info', 'Personal-details', 'Summary-item', 'Tab', 'Show/Hide Pane']
         },
         {name: 'Headers and Footers', state: 'components.headers', components: ['Govuk header', 'Govuk footer']},
         {name: 'Media', state: 'components.media', components: ['Image', 'statement']},
         {
             name: 'Navigation',
             state: 'components.navigation',
-            components: ['Action-pane', 'Breadcrumbs', 'Next/Previous Navigation']
+            components: ['Action-pane', 'Breadcrumbs', 'Next/Previous Navigation', 'Side Menu', 'Start Action Bar', 'Completed Action Bar']
         },
         {name: 'Timeline', state: 'components.timeline', components: ['Timeline']}
     ];
