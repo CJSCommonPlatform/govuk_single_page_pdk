39c886fe38493d85641e5928f12909f55f178196
Vygintas Ranonis <vygintas.ranonis@ext.soprasteria.com>
2016-08-02 15:57:59 +0100

    html/css tweaks to personal-details and statement components

--- src/modules/components/data-visualisation/personal-details/personal-details.component.ts
+++ src/modules/components/data-visualisation/personal-details/personal-details.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@govuk/angularjs-devtools';
 @Component({
   bindings: {
     title: '<',
+    subtitle: '<',
     info:  '<'
   },
   template: require('./personal-details.component.html')
