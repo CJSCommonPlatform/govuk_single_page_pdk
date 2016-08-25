16eb039697821bf7b0fb63f7fa5b4428b8fb25e0
Adam Kwiatek <a.kwiatek@kainos.com>
2016-08-25 16:06:12 +0100

    Configurable Skip to main content link
    Let 'Skip to main content' link to be optionally hidded. The link is
    displayed by default.

--- src/modules/components/headers-and-footers/header/header.component.ts
+++ src/modules/components/headers-and-footers/header/header.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@govuk/angularjs-devtools';
     heading:  '<',
     navItems: '<',
     showCookie: '<', // if true will display the cookie message over the header
+    showSkipLink: '<', // if true will display 'Skip to main content' link
     isBusiness: '<' // if true the width will expand to 1170px
   },
   template: require('./header.component.html')
