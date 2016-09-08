b25e74e4a44a9618d6cfd5ab12e2929a450531e4
joelemmer <joelemmer@hotmail.co.uk>
2016-09-08 14:49:19 +0100

    column-navigation added as per Trevor's markup

---
+++ src/modules/components/navigation/column-navigation/column-navigation.demo.ts
@@ -0,0 +1,141 @@
+import { Component } from '@govuk/angularjs-devtools';
+
+@Component({
+  template: require('./column-navigation.demo.html')
+})
+export class ColumnNavigationDemo {
+
+  example1 = {
+
+    column1: {
+
+      links: [
+        {
+          text: 'View this user\'s details',
+          onClick: function() {
+            console.log('Clicked \"Viewing the user\'s details\"');
+          }
+        },
+        {
+          text: 'Search for user in this organisation',
+          onClick: function() {
+            console.log('Clicked on \"Search for user in this organisation\"');
+          }
+        },
+        {
+          text: 'Search for another organisation',
+          onClick: function() {
+            console.log('Clicked on \"Search for another organisation\"');
+          }
+        },
+        {
+          text: 'More',
+          isBold: true,
+          onClick: function() {
+            console.log('Clicked on \"More\"');
+          }
+        }
+      ]
+    }
+  };
+
+  example2 = {
+
+    column1: {
+
+      metadata: {
+        text: 'This user last signed in on 13 December 2015 at 2:36pm'
+      },
+
+      links: [
+        {
+          text: 'Search for another user',
+          onClick: function() {
+            console.log('Clicked on \"Search for another user\"');
+          }
+        }
+      ]
+    },
+
+    column2: {
+
+      metadata: [
+        {
+          text: 'This user’s account will be suspended on 17 March 2017'
+        },
+        {
+          text: 'This user last signed in on 13 December 2015 at 2:36pm'
+        }
+      ],
+
+      links: [
+        {
+          text: 'Search for another user',
+          onClick: function() {
+            console.log('Clicked on \"Search for another user\"');
+          }
+        }
+      ]
+    }
+  };
+
+  example3 = {
+
+    column1: {
+
+      section1heading: 'Actions for this org',
+
+      section1links: [
+        {
+          text: 'View organisation\'s details',
+          onClick: function() {
+            console.log('Clicked on \"View organisation\'s details\"');
+          }
+        }
+      ],
+
+      section2heading: 'Other actions',
+
+      section2links: [
+        {
+          text: 'Search for another organisation',
+          onClick: function() {
+            console.log('Clicked on \"Search for another organisation\"');
+          }
+        },
+        {
+          text: 'Search all users',
+          onClick: function() {
+            console.log('Clicked on \"Search all users\"');
+          }
+        }
+      ]
+    },
+
+    column2: {
+
+      section1heading: 'To do',
+
+      section1links: [
+        {
+          text: 'View case material',
+          onClick: function() {
+            console.log('Clicked on \"View case material\"');
+          }
+        },
+        {
+          text: 'Enter analysis and charging decisions',
+          onClick: function() {
+            console.log('Clicked on \"Enter analysis and charging decisions\"');
+          }
+        },
+        {
+          text: 'Create action plan',
+          onClick: function() {
+            console.log('Clicked on \"Create action plan\"');
+          }
+        }
+      ]
+    }
+  };
+}
--- src/modules/components/navigation/navigation.ts
+++ src/modules/components/navigation/navigation.ts
@@ -2,6 +2,11 @@ import { ActionPaneComponent } from './action-pane/action-pane.component';
 import { ArrowComponent } from './arrow/arrow.component';
 import { BadgeItemComponent} from './badge-item/badge-item.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { ColumnNavigationComponent } from './column-navigation/column-navigation.component';
+import { ColumnNavigationSectionComponent } from './column-navigation/column-navigation-section.component';
+import { ColumnNavigationLinksComponent } from './column-navigation/column-navigation-links.component';
+import { ColumnNavigationRelatedMetadataComponent }
+                            from './column-navigation/column-navigation-related-metadata.component';
 import { CompletedBarComponent} from './completed-bar/completed-bar.component';
 import { NextPreviousNavigationComponent } from './next-previous-navigation/next-previous-navigation.component';
 import { SideMenuComponent} from './menus/side-menu/side-menu.component';
@@ -14,6 +19,10 @@ const module = angular.module('govuk-single-page-pdk.components.navigation', [])
   .component('govArrow', ArrowComponent)
   .component('govBadgeItem', BadgeItemComponent)
   .component('govBreadcrumbs', BreadcrumbsComponent)
+  .component('govColumnNavigation', ColumnNavigationComponent)
+  .component('govColumnNavigationSection', ColumnNavigationSectionComponent)
+  .component('govColumnNavigationLinks', ColumnNavigationLinksComponent)
+  .component('govColumnNavigationRelatedMetadata', ColumnNavigationRelatedMetadataComponent)
   .component('govCompletedBar', CompletedBarComponent)
   .component('govNextPreviousNavigation', NextPreviousNavigationComponent)
   .component('govSideMenu', SideMenuComponent)
