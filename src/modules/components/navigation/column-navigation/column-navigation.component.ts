32773626070fbfe1274fcf12c3007f874b350df4
joelemmer <joelemmer@hotmail.co.uk>
2016-09-08 14:49:19 +0100

    column-navigation added as per Trevor's markup

--- src/modules/components/navigation/column-navigation/column-navigation.component.ts
+++ src/modules/components/navigation/column-navigation/column-navigation.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@govuk/angularjs-devtools';
+import { isArray } from 'angular';
 
 @Component({
-  transclude: true,
+  bindings: {
+    sections: '<',
+    metadata: '<'
+  },
   template: require('./column-navigation.component.html')
 })
-export class ColumnNavigationComponent {}
+export class ColumnNavigationComponent {
+
+  metadata: any;
+
+  $onInit(): void {
+
+    /**
+     * The user can pass in a single metadata object rather than
+     * an array of metadata objects if they want, but it is converted
+     * here to an array.
+     */
+    if (this.metadata && !isArray(this.metadata)) {
+      this.metadata = [this.metadata];
+    }
+  }
+}
