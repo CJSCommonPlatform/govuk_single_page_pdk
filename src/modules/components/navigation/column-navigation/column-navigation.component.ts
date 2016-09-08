f728854dceec999ab400f5f959df2d2328c83112
joelemmer <joelemmer@hotmail.co.uk>
2016-09-08 14:49:19 +0100

    column-navigation added as per Trevor's markup

---
+++ src/modules/components/navigation/column-navigation/column-navigation.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@govuk/angularjs-devtools';
+
+@Component({
+  transclude: true,
+  template: require('./column-navigation.component.html')
+})
+export class ColumnNavigationComponent {}
