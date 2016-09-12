2361f5ce945a4047b97386b28513d9cbe723c9df
joelemmer <joelemmer@hotmail.co.uk>
2016-09-12 15:33:02 +0100

    Added document-download component following Trevor's markup

---
+++ src/modules/components/media/download-document/download-document.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@govuk/angularjs-devtools';
+import { noFileType } from '../filters/noFileType.filter';
+
+@Component({
+  bindings: {
+    // Optional attribute. The url of an image.
+    // If provided, a thumbnail of the image will be shown instead of an icon.
+    imageSrc:    '<?',
+    // String for the date or datetime.
+    // Use of the '@' binding means that the attribute can either
+    // render out a string e.g. datetime="{{ myDateString }}"
+    // or can format a JavaScript date to be a date or datetime
+    // datetime="{{ myJavaScriptDate | date:'dd MMMM yyyy' }}"
+    // datetime="{{ myJavaScriptDate | date:'dd MMMM yyyy hh:mm a }}"
+    datetime:         '@',
+    // Number. The number of pages the document contains.
+    numPages:        '<',
+    // Number. The filesize in bytes
+    fileSize:        '<',
+    // String. The file name including filetype suffix e.g. 'important_doc.pdf'
+    // Unless overridden by the title attribute, the filename less the 
+    // filetype suffix will be used as the title e.g. in this case 'important_doc'
+    fileName:        '<',
+    // Optional attribute. String.
+    // Text to display for the title link. Will override the filename.
+    title:           '<?',
+    // The onclick handler (presumably to download the document)
+    onSelect:        '&',
+    // Optional attribute. Boolean
+    // Whether to use the small icon. Defaults to false.
+    smallIcon:       '<?'
+  },
+  template: require('./download-document.component.html')
+})
+export class DownloadDocumentComponent {
+
+  static $inject = ['$filter'];
+
+  title: string;
+  fileName: string;
+
+  constructor(private $filter: any) {}
+
+  $onInit(): void {
+
+    // Providing title is option.
+    // If not provided, default to file name
+    if (!this.title) {
+      // Chops off the file type suffix from the provided filename
+      this.title = this.$filter('noFileType')(this.fileName);
+    }
+
+  }
+}
