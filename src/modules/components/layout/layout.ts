6e8e67b24e8d9263744447de5f13974a306cd93b
louisbichard <contact@louisjohnbichard.co.uk>
2017-12-11 11:51:50 +0000

    :sparkles: Addeed link box to the masthead

--- src/modules/components/layout/layout.ts
+++ src/modules/components/layout/layout.ts
@@ -1,9 +1,12 @@
 import { DividerComponent } from './divider/divider.component';
 import { MastheadComponent } from './masthead/masthead.component';
+import { MastheadLinkBox } from './masthead-link-box/masthead-link-box.component';
 
 const module = angular.module('govuk-single-page-pdk.components.layout', [])
 
   .component('pdkMasthead', MastheadComponent)
+  .component('mastheadLinkBox', MastheadLinkBox)
+
   .component('pdkDivider', DividerComponent);
 
 export default module.name;
