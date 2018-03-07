b61b4d1a935a4899ccf89f39f2d1e959baffc939
Ewan Tavener <ewan.tavener@hmcts.net>
2018-03-07 14:03:06 +0000

    correct file paths

--- src/modules/components/navigation/menus/global-navigation/top-menu.demo.ts
+++ src/modules/components/navigation/menus/global-navigation/top-menu.demo.ts
@@ -1,4 +1,4 @@
-import { Component } from '../../../../util/angular-utils';
+import { Component } from '../../../../../util/angular-utils';
 
 @Component({
   template: require('./top-menu.demo.html')
